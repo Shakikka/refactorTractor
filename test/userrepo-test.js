ead99c5b0ae04a79e8cfe90fbed9c77702dccbd3
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2021-02-24 17:21:02 -0700

    Fix test requirements, imports, variables

--- test/userrepo-test.js
+++ test/userrepo-test.js
@@ -2,36 +2,21 @@ import { expect } from 'chai';
 
 import UserRepo from '../src/User-repo';
 import User from '../src/User';
+import {
+  fakeUserData
+} from '../src/data/fakeData';
 
+describe('User Repo', function () {
 
-describe('User Repo', function() {
-  let user1;
-  let user2;
-  let user;
-  let userRepo;
+  let userOne, userTwo, users, userRepo;
 
-  beforeEach(function() {
-    user1 = new User({
-      id: 1,
-      name: "Alex Roth",
-      address: "1234 Turing Street, Denver CO 80301-1697",
-      email: "alex.roth1@hotmail.com",
-      strideLength: 4.3,
-      dailyStepGoal: 10000,
-      friends: [2, 3, 4]
-    });
-    user2 = new User({
-      id: 2,
-      name: "Allie McCarthy",
-      address: "1235 Turing Street, Denver CO 80301-1697",
-      email: "allie.mcc1@hotmail.com",
-      strideLength: 3.3,
-      dailyStepGoal: 9000,
-      friends: [1, 3, 4]
-    });
-    users = [user1, user2];
+  beforeEach(function () {
+    userOne = new User(fakeUserData[0]);
+    userTwo = new User(fakeUserData[1]);
+    users = [userOne, userTwo];
     userRepo = new UserRepo(users);
   });
+
 
   it('should be a function', function() {
     const userRepo = new UserRepo();
