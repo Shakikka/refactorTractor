ee571073f9840941bd0b52b20fdd86c1cec6d1e9
JS <jeff.shep777@gmail.com>
2021-03-03 11:29:52 -0700

    Remove old code from api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,3 @@
-//GET requests
-
 const checkIfError = response => {
   if (!response.ok) {
     throw new Error('An error has been encountered. Please try again.');
@@ -7,7 +5,6 @@ const checkIfError = response => {
     return response.json();
   }
 }
-
 
 export const sleepDataAPI = fetch("http://localhost:3001/api/v1/sleep")
   .then(checkIfError)
@@ -25,9 +22,6 @@ export const hydrationDataAPI = fetch("http://localhost:3001/api/v1/hydration")
   .then(checkIfError)
   .catch(err => alert(err))
 
-
-//POST requests
-
 export const postNewData = (path, body) => {
   return fetch(`http://localhost:3001/api/v1/${path}`, {
     method: 'POST',
@@ -37,34 +31,3 @@ export const postNewData = (path, body) => {
     body: JSON.stringify(body)
   })
 }
-
-
-// export const sleepDataPost = (body) => {
-//   return fetch("http://localhost:3001/api/v1/sleep", {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(body)
-//   })
-// }
-
-// export const hydrationDataPost = (body) => {
-//   return fetch("http://localhost:3001/api/v1/hydration", {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(body)
-//   })
-// }
-
-// export const activityDataPost = (dataFormEntry) => {
-//   return fetch("http://localhost:3001/api/v1/activity", {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(dataFormEntry)
-//   })
-// }
